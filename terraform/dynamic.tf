a9f3bc9701a2bd6d0d2e1bc1838eed6725000588
Adamcoakley <Adamcoakley18@gmail.com>
2023-01-26 20:19:45 +0000

    updated

--- terraform/dynamic.tf
+++ terraform/dynamic.tf
@@ -1,5 +1,5 @@
 # Populate inventory file with dynamic ips
 resource "local_file" "dynamic_inventory" {
   content  = "[webservers]\n ${aws_instance.group-two-server.public_ip}\n"
-  filename = "./inventory.yaml"
+  filename = "../inventory.yaml"
 }
