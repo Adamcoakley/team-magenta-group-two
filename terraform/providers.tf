791830584d45e1d00724633c2e8b4d8fe2e5dd9e
Adamcoakley <Adamcoakley18@gmail.com>
2023-01-26 19:16:25 +0000

    updated

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-    shared_credentials_files = ["~/.aws/credentials"]
-    # shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+    # shared_credentials_files = ["~/.aws/credentials"]
+    shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
     region = "eu-west-1"
 }
